4e10318e4e017812544a234772b375f097105cdb
christine ben <christinrbenabdallah@gmail.com>
2024-01-23 13:25:28 +0100

    deploy4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './page/About';
 import PageErreur from './page/Erreur';
 import Home from './page/Home';
 import HomePage from './page/HomePage';
 
-
-
 function App() {
   return (
-   
-    <BrowserRouter >
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route exact path="/home" element={<Home />} />
-                <Route exact path="/about" element={<About />} />
-                <Route exact path="/HomePage/:id" element={<HomePage />} />
-                <Route path="*" element={<PageErreur/>} />
-            </Routes>
-        </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/HomePage/:id" element={<HomePage />} />
+        <Route path="*" element={<PageErreur />} />
+      </Routes>
+    </Router>
   );
 }
 
